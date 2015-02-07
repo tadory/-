e4e55a710bd629711ef0edd8546c3afc5931e494
tadory <Kengouryuuu@softbank.ne.jp>
2015-02-07 23:40:02 +0900

    Initial Commit

---
+++ 賞味期限/賞味期限/賞味期限/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  賞味期限
+//
+//  Created by 田所　龍 on 2015/02/07.
+//  Copyright (c) 2015年 田所　龍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
