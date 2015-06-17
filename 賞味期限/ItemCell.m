da42f64b6cae6d0fac4d6e3a0b43a2a6f0a9475b
tadory <Kengouryuuu@softbank.ne.jp>
2015-06-17 20:53:15 +0900

    Edit some codes

---
+++ 賞味期限/ItemCell.m
@@ -0,0 +1,13 @@
+//
+//  ItemCell.m
+//  賞味期限
+//
+//  Created by 田所　龍 on 2015/02/26.
+//  Copyright (c) 2015年 田所　龍. All rights reserved.
+//
+
+#import "ItemCell.h"
+
+@implementation ItemCell
+
+@end
